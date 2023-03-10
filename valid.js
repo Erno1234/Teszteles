cfd7a5688733d06de5c0733ce064932060ffd1da
Erno1234 <palocierno12@gmail.com>
2023-03-10 11:13:37 +0100

    próba

--- valid.js
+++ valid.js
@@ -1,8 +1,16 @@
-window.addEventListener("load", validateForm)
 function validateForm() {
     let x = document.validTeszt.kNev.value;
     if (x === "") {
         alert("A mező kitöltése kötelező!");
         return false;
     }
+    let jelszo = document.validTeszt.jelszo.value;
+    let jelszoK = document.validTeszt.jelszo.value;
+    if (jelszo !== jelszoK){
+        alert("Nem egyeznek a jelszavak!");
+        jelszoK.focus();
+        return false;
+
+    }
+    
 }
